efee7a2ac1993ce44a81cda7b5e0af5fcec3dbbe
bnkosi2019 <47521522+bnkosi2019@users.noreply.github.com>
2020-01-02 13:37:31 +0200

    i dunno , these imports are all wonky

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
 import numpy
-from tensorflow import *
-from tensorflow.python.compiler.tensorrt import trt_convert as trt
+# from tensorflow import *
+import tflearn
+# from tensorflow.python.compiler.tensorrt import trt
+import tensorflow.contrib.tensorrt
 import tensorflow
 import random
 import json
@@ -15,7 +17,8 @@ with open("intents.json") as file:
     
 words = []
 labels  = []
-docs = []
+docs_x = []
+docs_y = []
 
 #data preprocessing
 
@@ -23,8 +26,50 @@ for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs.append(pattern)
+        docs_x.append(wrds)
+        docs_y.append(intent["tag"])
         
         if intent["tag"] not in labels:
             labels.append(intent["tag"])
-        
+            
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+words - sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for doc in enumerate(docs_x):
+    bag = []
+    wrds = [stemmer.stem(w) for w in doc]
+    
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+            
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+    
+    training.append(bag)
+    output.append(output_row)
+    
+training = numpy.array(training)
+output = numpy.array(output)
+
+tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None,len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("model.tflearn")
